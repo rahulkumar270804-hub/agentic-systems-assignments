4420d6100a939122cb2815c18689874e4c0fd98f
rahulkumar270804-hub <rahulkumar270804@gmail.com>
2026-03-10 05:56:01 +0000

    Added FastAPI search endpoint query parameters

---
+++ fastapi-fundamentals/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from typing import Optional
+
+app = FastAPI()
+
+@app.get("/search")
+def search(name: Optional [str] = None, age: Optional [int] = None):
+    return {
+        "name": name, 
+        "age": age
+        }
+
+   
